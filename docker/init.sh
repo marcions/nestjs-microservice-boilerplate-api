78474b5963df79008104fe4b896a2699af8e2719
Marcio Sales <marcionsales@hotmail.com>
2024-01-18 15:02:31 -0300

    Docker Adjusts

--- docker/init.sh
+++ docker/init.sh
@@ -10,3 +10,5 @@ docker volume create mysql-data
 docker volume create sonarqube-data
 
 docker-compose -f docker/docker-compose-infra.yml up -d --build --remove-orphans
+
+docker exec mongo ./mongo/rs-init.sh
