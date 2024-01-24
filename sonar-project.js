9989c6ea33348e4651783da4929d377ea32b1da9
Marcio Sales <marcionsales@hotmail.com>
2024-01-24 10:52:10 -0300

    Sonar

---
+++ sonar-project.js
@@ -0,0 +1,15 @@
+import sonarqubeScanner from 'sonarqube-scanner';
+sonarqubeScanner(
+  {
+    serverUrl: 'http://localhost:9000',
+    options: {
+      'sonar.sources': 'src',
+      'sonar.tests': 'src',
+      'sonar.inclusions': '**',
+      'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+      'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml'
+    }
+  },
+  () => {}
+);
